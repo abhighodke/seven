ba514963b3bbbaa5ed70a9e3ff1b5917c1707388
Abhishek Ghodke <abhighodke3@gmail.com>
2024-08-31 13:35:07 +0530

    client updated

--- src/clientData.js
+++ src/clientData.js
@@ -1,37 +1,31 @@
 // src/clientData.js
 const clientData = {
-    headerInitials: "JP", // Header Initials
-    heroTagline: "Judy Patch Realty", // Hero Tagline
-    agentName: "Judy Patch",
-    aboutMeShort: "Judy Patch has been a real estate professional for over 20 years, helping her clients across various markets to sell, purchase, and rent properties by offering a full spectrum of client-oriented services designed to match all their real estate goals. As a well-established and a high achiever who generally ranks in the top 20% of agents for sales volume, Judy is also a member of the coveted Multi-Million Dollar Club. Judy is the epitome of hard work and sheer determination driving a passionate desire to serve others and make their dreams come true.", // AboutMe Short Description
-    aboutMeDetailed: `Judy Patch has been a real estate professional for over 20 years, helping her clients across various markets to sell, purchase, and rent properties by offering a full spectrum of client-oriented services designed to match all their real estate goals. As a well-established and a high achiever who generally ranks in the top 20% of agents for sales volume, Judy is also a member of the coveted Multi-Million Dollar Club. Judy is the epitome of hard work and sheer determination driving a passionate desire to serve others and make their dreams come true.
+    headerInitials: "CCC",
+    heroTagline: "Christi Cooper Collins Realty",
+    agentName: "Christi Cooper Collins",
+    aboutMeShort: "I specialize in finding perfect homes for buyers and sellers, including investors and veterans. My expertise and dedication to exceptional service guide you to the best decisions. Let's find your ideal home together.",
+    aboutMeDetailed: `It is my passion to assist clients with reaching their goal of owning a home or selling their current home to find a new one. It is important to me that my clients find the ideal home to match their needs and their budget. Also, I have helped many investors find the perfect property to complete their investment portfolio. 
 
-Prior to joining the real estate industry, Judy had a successful career in the banking industry. Having worked in accounting, finance, mortgage, and consumer lending, she possesses extensive knowledge and understanding of mortgage lending and is able to guide clients accordingly on matters related to the funding of their properties. She is adept at negotiating, marketing and is able to find the balance between budget and property interests, using a results-driven approach that is customized to fit each client base.
+With my knowledge and experience I provide my clients accurate and complete detailed information so they can make the best decisions. Also, with being certified to help our Veterans and their families in finding a home and utilizing their full VA eligibility. Allow me to assist you with your real estate needs.
 
-Judy is driven and focused, yet fun, genuine, and very caring. This fine contrast puts her clients at ease as she implements her professional and personable working style to advocate for them. She is very organized with a keen eye for details and always goes out of her way to ensure that clients experience a high level of commitment and performance. She has developed an efficient system for high-level client services in order to achieve success for all those she works with and enjoys connecting with people and fostering lasting relationships based on open communication and trust.
-
-Aside from work, Judy enjoys spending time with family and boating. She can also be found playing tennis, golfing, working out, or learning new things such as playing guitar and painting.`, // AboutMe Detailed Description
-    testimonials: [
-        { quote: "Working with Judy was a game-changer for us. Her deep knowledge of the New York market and personalized approach made finding our dream home effortless. Highly recommended for anyone seeking a top-notch real estate experience.", author: "John and Emily R." },
-        { quote: "Judy Patch Realty provided exceptional service from start to finish. The team’s professionalism and attention to detail ensured that our property sale was smooth and successful. We couldn’t have asked for a better partner in real estate.", author: "Michael S." },
-        { quote: "The dedication and expertise of Judy were evident throughout our home-buying journey. Her ability to understand our needs and navigate the competitive market made the process enjoyable and stress-free. We are thrilled with our new home!", author: "Sarah and David L." },
-        { quote: "Judy and her team are truly outstanding. Their market knowledge, responsiveness, and client-focused approach set them apart. We have bought and sold multiple properties with them, and each experience has been exceptional.", author: "Jessica and Mark T." },
-        { quote: "If you’re looking for a real estate agent who goes above and beyond, Judy is the one. Her dedication to finding the perfect property and ensuring a smooth transaction is unmatched. We are incredibly satisfied with the service we received.", author: "Laura K." }
-    ], // Testimonials
-    footerTagline: "Your Trusted Partner in Luxury Real Estate", // Footer Tagline
-    email: "judypatch1@gmail.com", // Email
-    address: "100 W PACES FERRY RD NW, ATLANTA, GA, 30305", // Address
-    phone: "678-315-6079", // Phone Number
-    footerDescription: "Judy Patch Realty is committed to delivering the highest standard of real estate services. Whether you're buying, selling, or investing in luxury properties, we offer unparalleled expertise and a personalized approach to meet your needs.", // Footer Description
+Customer Service excellence is our Passion!`,
+    testimonials: [{"quote": "Working with Christi Cooper Collins was a game-changer for us. Her deep knowledge of the New York market and personalized approach made finding our dream home effortless. Highly recommended for anyone seeking a top-notch real estate experience.", "author": "John and Emily R."}, {"quote": "Christi Cooper Collins provided exceptional service from start to finish. The teams professionalism and attention to detail ensured that our property sale was smooth and successful. We could not have asked for a better partner in real estate.", "author": "Michael S."}, {"quote": "The dedication and expertise of Christi Cooper Collins were evident throughout our home-buying journey. Her ability to understand our needs and navigate the competitive market made the process enjoyable and stress-free. We are thrilled with our new home!", "author": "Sarah and David L."}, {"quote": "Christi Cooper Collins and Her team are truly outstanding. Their market knowledge, responsiveness, and client-focused approach set them apart. We have bought and sold multiple properties with them, and each experience has been exceptional.", "author": "Jessica and Mark T."}, {"quote": "If you are looking for a real estate agent who goes above and beyond, Christi Cooper Collins is the one. Her dedication to finding the perfect property and ensuring a smooth transaction is unmatched. We are incredibly satisfied with the service we received.", "author": "Laura K."}],
+    footerTagline: "Your Trusted Partner in Luxury Real Estate",
+    email: "",
+    address: "",
+    phone: "(205) 478-2525",
+    footerDescription: `Christi Cooper Collins Realty is committed to delivering the highest standard of real estate services. Whether you're buying, selling, or investing in luxury properties, we offer unparalleled expertise and a personalized approach to meet your needs.`,
     socialMediaLinks: {
-        facebook: "",
+        facebook: "https://www.facebook.com/christicoopercollins/",
         Instagram: "",
         linkedin: "",
-    }, // Social Media Links
+    },
     contactDetails: {
-        email: "judypatch1@gmail.com",
-        phone: "678-315-6079",
-    }, // Contact Details
+        email: "",
+        phone: "(205) 478-2525",
+    },
+    website: "",
+    dp: "https://scontent-sjc3-1.xx.fbcdn.net/v/t39.30808-6/259741952_10223080416373731_7041932489179953820_n.jpg?_nc_cat=101&ccb=1-7&_nc_sid=6ee11a&_nc_ohc=0_cTkrhv0dsQ7kNvgHhG3Wz&_nc_ht=scontent-sjc3-1.xx&oh=00_AYB4GYZyaiwsEeDysR0-ntciVmL0pfK9D0NsXc-_uUQYhA&oe=66D7DE21" // Agent image URL
 };
 
 export default clientData;
