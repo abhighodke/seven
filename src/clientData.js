3e79f51a8bcf2f5a0e8347e134f6722dce5bd66e
Abhishek Ghodke <abhighodke3@gmail.com>
2024-09-04 21:37:37 +0530

    client updated

--- src/clientData.js
+++ src/clientData.js
@@ -1,31 +1,31 @@
 // src/clientData.js
 const clientData = {
-    headerInitials: "CCC",
-    heroTagline: "Christi Cooper Collins Realty",
-    agentName: "Christi Cooper Collins",
+    headerInitials: "HSS",
+    heroTagline: "Holly Smith Sanders Realty",
+    agentName: "Holly Smith Sanders",
     aboutMeShort: "I specialize in finding perfect homes for buyers and sellers, including investors and veterans. My expertise and dedication to exceptional service guide you to the best decisions. Let's find your ideal home together.",
     aboutMeDetailed: `It is my passion to assist clients with reaching their goal of owning a home or selling their current home to find a new one. It is important to me that my clients find the ideal home to match their needs and their budget. Also, I have helped many investors find the perfect property to complete their investment portfolio. 
 
 With my knowledge and experience I provide my clients accurate and complete detailed information so they can make the best decisions. Also, with being certified to help our Veterans and their families in finding a home and utilizing their full VA eligibility. Allow me to assist you with your real estate needs.
 
 Customer Service excellence is our Passion!`,
-    testimonials: [{"quote": "Working with Christi Cooper Collins was a game-changer for us. Her deep knowledge of the New York market and personalized approach made finding our dream home effortless. Highly recommended for anyone seeking a top-notch real estate experience.", "author": "John and Emily R."}, {"quote": "Christi Cooper Collins provided exceptional service from start to finish. The teams professionalism and attention to detail ensured that our property sale was smooth and successful. We could not have asked for a better partner in real estate.", "author": "Michael S."}, {"quote": "The dedication and expertise of Christi Cooper Collins were evident throughout our home-buying journey. Her ability to understand our needs and navigate the competitive market made the process enjoyable and stress-free. We are thrilled with our new home!", "author": "Sarah and David L."}, {"quote": "Christi Cooper Collins and Her team are truly outstanding. Their market knowledge, responsiveness, and client-focused approach set them apart. We have bought and sold multiple properties with them, and each experience has been exceptional.", "author": "Jessica and Mark T."}, {"quote": "If you are looking for a real estate agent who goes above and beyond, Christi Cooper Collins is the one. Her dedication to finding the perfect property and ensuring a smooth transaction is unmatched. We are incredibly satisfied with the service we received.", "author": "Laura K."}],
+    testimonials: [{"quote": "Working with Holly Smith Sanders was a game-changer for us. Her deep knowledge of the New York market and personalized approach made finding our dream home effortless. Highly recommended for anyone seeking a top-notch real estate experience.", "author": "John and Emily R."}, {"quote": "Holly Smith Sanders provided exceptional service from start to finish. The teams professionalism and attention to detail ensured that our property sale was smooth and successful. We could not have asked for a better partner in real estate.", "author": "Michael S."}, {"quote": "The dedication and expertise of Holly Smith Sanders were evident throughout our home-buying journey. Her ability to understand our needs and navigate the competitive market made the process enjoyable and stress-free. We are thrilled with our new home!", "author": "Sarah and David L."}, {"quote": "Holly Smith Sanders and Her team are truly outstanding. Their market knowledge, responsiveness, and client-focused approach set them apart. We have bought and sold multiple properties with them, and each experience has been exceptional.", "author": "Jessica and Mark T."}, {"quote": "If you are looking for a real estate agent who goes above and beyond, Holly Smith Sanders is the one. Her dedication to finding the perfect property and ensuring a smooth transaction is unmatched. We are incredibly satisfied with the service we received.", "author": "Laura K."}],
     footerTagline: "Your Trusted Partner in Luxury Real Estate",
-    email: "",
-    address: "",
-    phone: "(205) 478-2525",
-    footerDescription: `Christi Cooper Collins Realty is committed to delivering the highest standard of real estate services. Whether you're buying, selling, or investing in luxury properties, we offer unparalleled expertise and a personalized approach to meet your needs.`,
+    email: "holly@SoldByStratford.com",
+    address: "Vestavia Hills, AL, United States",
+    phone: "205-215-7144",
+    footerDescription: `Holly Smith Sanders Realty is committed to delivering the highest standard of real estate services. Whether you're buying, selling, or investing in luxury properties, we offer unparalleled expertise and a personalized approach to meet your needs.`,
     socialMediaLinks: {
-        facebook: "https://www.facebook.com/christicoopercollins/",
+        facebook: "https://www.facebook.com/HollySmithSanders",
         Instagram: "",
         linkedin: "",
     },
     contactDetails: {
-        email: "",
-        phone: "(205) 478-2525",
+        email: "holly@SoldByStratford.com",
+        phone: "205-215-7144",
     },
     website: "",
-    dp: "https://scontent-sjc3-1.xx.fbcdn.net/v/t39.30808-6/259741952_10223080416373731_7041932489179953820_n.jpg?_nc_cat=101&ccb=1-7&_nc_sid=6ee11a&_nc_ohc=0_cTkrhv0dsQ7kNvgHhG3Wz&_nc_ht=scontent-sjc3-1.xx&oh=00_AYB4GYZyaiwsEeDysR0-ntciVmL0pfK9D0NsXc-_uUQYhA&oe=66D7DE21" // Agent image URL
+    dp: "https://scontent-sjc3-1.xx.fbcdn.net/v/t39.30808-6/246450662_948582982425193_6931715985024692853_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=6ee11a&_nc_ohc=MGHavO1m9Z4Q7kNvgHHaAol&_nc_ht=scontent-sjc3-1.xx&oh=00_AYBAjY5Y6Ubat2-HV3iUjB79VzpCnFDdnfmFBenpCuFidA&oe=66DBAABD" // Agent image URL
 };
 
 export default clientData;
